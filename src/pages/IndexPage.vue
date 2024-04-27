3cb64f0b59cb01a6222ab84d8851447c65212173
TakumiHendricksDev <takumihendricksdev@gmail.com>
2024-04-26 21:45:05 -0500

    Setup firebase

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,15 @@
 <template>
   <q-page class="q-pa-xl q-mt-xl">
-    <div>
-      <p class="text-h1 text-weight-bold letter-space-lg q-mb-sm">I'm Takumi</p>
-      <p class="text-h1 text-weight-bold letter-space-lg q-mb-sm">Hendricks</p>
+    <div class="card">
+      <p class="text-h1 text-weight-bold letter-space-lg q-mb-sm text-grey-9">I'm Takumi</p>
+      <p class="text-h1 text-weight-bold letter-space-lg q-mb-sm text-grey-9">Hendricks</p>
       <p class="text-h1 text-weight-bold letter-space-xl text-indigo">Software Engineer</p>
+      <p class="text-h5 text-gray-8">
+        I'm a software engineer with a passion for building web applications.
+        <br/>I'm proficient in frameworks such as Django, Vue.js, and React
+      </p>
     </div>
-    <p class="text-h4">
-      I'm a software engineer with a passion for building web applications. I'm
-      proficient in frameworks such as Django, Vue.js, and React
-    </p>
+
   </q-page>
 </template>
 
