bc4f10425267319935114f8805793f83be6b41b1
TakumiHendricksDev <takumihendricksdev@gmail.com>
2024-05-13 20:18:43 -0500

    comment newsletter

--- src/components/RecentPost.vue
+++ src/components/RecentPost.vue
@@ -2,13 +2,13 @@
   <div class="bg-grey-10 q-py-xl q-px-xl shadow-10">
     <div class="flex q-mb-lg">
       <div class="text-h4 text-bold text-primary q-mr-lg">Recent Post</div>
-      <q-btn
-        color="grey-9"
-        label="Subscribe to my newsletter"
-        icon="mail"
-        size="md"
-        @click="prompt = true"
-      />
+<!--      <q-btn-->
+<!--        color="grey-9"-->
+<!--        label="Subscribe to my newsletter"-->
+<!--        icon="mail"-->
+<!--        size="md"-->
+<!--        @click="prompt = true"-->
+<!--      />-->
     </div>
     <hr class="q-mb-lg" />
     <div class="flex">
@@ -21,22 +21,22 @@
     </div>
   </div>
 
-  <q-dialog v-model="prompt" persistent>
-      <q-card style="min-width: 350px">
-        <q-card-section>
-          <div class="text-h6">Your Email Address</div>
-        </q-card-section>
+<!--  <q-dialog v-model="prompt" persistent>-->
+<!--      <q-card style="min-width: 350px">-->
+<!--        <q-card-section>-->
+<!--          <div class="text-h6">Your Email Address</div>-->
+<!--        </q-card-section>-->
 
-        <q-card-section class="q-pt-none q-mb-lg">
-          <q-input filled type="email" v-model="emailAddress" autofocus @keyup.enter="prompt = false" />
-        </q-card-section>
+<!--        <q-card-section class="q-pt-none q-mb-lg">-->
+<!--          <q-input filled type="email" v-model="emailAddress" autofocus @keyup.enter="prompt = false" />-->
+<!--        </q-card-section>-->
 
-        <q-card-actions align="right" class="text-primary">
-          <q-btn dark-percentage color="dark" label="Cancel" v-close-popup />
-          <q-btn dark-percentage color="dark" label="Add to newsletter" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+<!--        <q-card-actions align="right" class="text-primary">-->
+<!--          <q-btn dark-percentage color="dark" label="Cancel" v-close-popup />-->
+<!--          <q-btn dark-percentage color="dark" label="Add to newsletter" v-close-popup />-->
+<!--        </q-card-actions>-->
+<!--      </q-card>-->
+<!--    </q-dialog>-->
 </template>
 
 <script setup>
